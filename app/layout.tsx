be766077a103a32bf8c1cbd2b14479e8f58589ab
Mark Slorach <hello@markslorach.com>
2024-05-24 11:22:13 +0100

    installed shadcn/ui

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,11 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "@radix-ui/themes/styles.css";
-import './theme-config.css'
 import "./globals.css";
 import NavBar from "./NavBar";
-import { Theme, ThemePanel } from "@radix-ui/themes";
 
 const inter = Inter({
   subsets: ['latin'],
   display: 'swap',
-  variable: '--font-inter',
 });
 
 export const metadata: Metadata = {
@@ -25,11 +21,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Theme radius="small">
+
           <NavBar />
           <main className="p-5">{children}</main>
-          {/* <ThemePanel /> */}
-        </Theme>
       </body>
     </html>
   );
