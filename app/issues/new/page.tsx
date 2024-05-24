8396c125587ce7e1a4a0bcad0bbb9aaa8980fbf9
Mark Slorach <hello@markslorach.com>
2024-05-24 13:19:38 +0100

    handled form submit errors

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -2,10 +2,13 @@
 import { useRouter } from "next/navigation";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+import { AlertCircle } from "lucide-react";
 import { useForm, Controller } from "react-hook-form";
 import SimpleMDE from "react-simplemde-editor";
 import "easymde/dist/easymde.min.css";
 import axios from "axios";
+import { useState } from "react";
 
 interface IssueForm {
   title: string;
@@ -16,25 +19,43 @@ const NewIssuePage = () => {
   const router = useRouter();
   const { register, control, handleSubmit } = useForm<IssueForm>();
 
-  return (
-    <form
-      onSubmit={handleSubmit(async (data) => {
-        await axios.post("/api/issues", data);
-        router.push("/issues");
-      })}
-      className="max-w-xl space-y-3"
-    >
-      <Input placeholder="Title" {...register("title")} />
-      <Controller
-        name="description"
-        control={control}
-        render={({ field }) => (
-          <SimpleMDE placeholder="Description" {...field} />
-        )}
-      />
+  const [error, setError] = useState("");
 
-      <Button>Submit New Issue</Button>
-    </form>
+  return (
+    <div className="max-w-xl space-y-3">
+      {error && (
+        <Alert variant="destructive">
+          <AlertCircle className="h-4 w-4" />
+          <AlertTitle>Error</AlertTitle>
+          <AlertDescription>
+            {error}
+          </AlertDescription>
+        </Alert>
+      )}
+      <form
+        onSubmit={handleSubmit(async (data) => {
+          try {
+            await axios.post("/api/issues", data);
+            router.push("/issues");
+          } catch (error) {
+            setError("An error occurred. Please try again.");
+          }
+        })}
+        className="space-y-3"
+      >
+        <Input placeholder="Title" {...register("title")} />
+
+        <Controller
+          name="description"
+          control={control}
+          render={({ field }) => (
+            <SimpleMDE placeholder="Description" {...field} />
+          )}
+        />
+
+        <Button>Submit New Issue</Button>
+      </form>
+    </div>
   );
 };
 
