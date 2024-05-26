4f0bdecb1180ea7d2397de8be4da65490bb33a50
Mark Slorach <hello@markslorach.com>
2024-05-26 23:13:07 +0100

    markdown preview

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -2,6 +2,7 @@ import IssueStatusBadge from "@/app/components/IssueStatusBadge";
 import { Card } from "@/components/ui/card";
 import prisma from "@/prisma/client";
 import { notFound } from "next/navigation";
+import ReactMarkdown from "react-markdown";
 
 const IssueDetailPage = async ({ params }: { params: { id: string } }) => {
   const issue = await prisma.issue.findUnique({
@@ -11,14 +12,14 @@ const IssueDetailPage = async ({ params }: { params: { id: string } }) => {
   if (!issue) notFound();
 
   return (
-    <div className="space-y-3">
+    <div className="space-y-4">
       <h1 className="text-2xl font-bold">{issue.title}</h1>
       <div className="flex space-x-2">
         <IssueStatusBadge status={issue.status} />
         <p>{issue.createdAt.toDateString()}</p>
       </div>
-      <Card className="p-3">
-        <p>{issue.description}</p>
+      <Card className="p-3 prose">
+        <ReactMarkdown>{issue.description}</ReactMarkdown>
       </Card>
     </div>
   );
