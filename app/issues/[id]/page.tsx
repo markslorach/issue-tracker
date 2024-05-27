2742304a14de24153e3a7c54eaf10e62e6430734
Mark Slorach <hello@markslorach.com>
2024-05-27 11:19:28 +0100

    added edit button

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,8 +1,11 @@
 import IssueStatusBadge from "@/app/components/IssueStatusBadge";
+import { Button } from "@/components/ui/button";
 import { Card } from "@/components/ui/card";
 import prisma from "@/prisma/client";
 import { notFound } from "next/navigation";
 import ReactMarkdown from "react-markdown";
+import { FilePenLine } from 'lucide-react';
+import Link from "next/link";
 
 const IssueDetailPage = async ({ params }: { params: { id: string } }) => {
   const issue = await prisma.issue.findUnique({
@@ -12,15 +15,23 @@ const IssueDetailPage = async ({ params }: { params: { id: string } }) => {
   if (!issue) notFound();
 
   return (
-    <div className="space-y-4 max-w-xl">
+    <div className="grid lg:grid-cols-2 gap-5">
+
+    <div className="space-y-4">
       <h1 className="text-2xl font-bold">{issue.title}</h1>
       <div className="flex space-x-2">
         <IssueStatusBadge status={issue.status} />
         <p>{issue.createdAt.toDateString()}</p>
       </div>
-      <Card className="p-3 prose">
+      <Card className="px-3 py-6 prose">
         <ReactMarkdown>{issue.description}</ReactMarkdown>
       </Card>
+    </div>
+    <div>
+      <Link href={`/issues/${issue.id}/edit`}>
+      <Button><FilePenLine className="h-4 w-4 mr-2"/>Edit Issue</Button>
+      </Link>
+    </div>
     </div>
   );
 };
