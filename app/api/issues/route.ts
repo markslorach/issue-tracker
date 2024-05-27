8b538c218e745c5d2414ab8091b981afe34397f4
Mark Slorach <hello@markslorach.com>
2024-05-27 08:37:39 +0100

    built api for updating an issue

--- app/api/issues/route.ts
+++ app/api/issues/route.ts
@@ -1,12 +1,12 @@
 import prisma from "@/prisma/client";
 import { NextRequest, NextResponse } from "next/server";
-import { createIssueSchema } from "@/app/validationSchemas";
+import { issueSchema } from "@/app/validationSchemas";
 
 export async function POST(request: NextRequest) {
   // Create request body
   const body = await request.json();
   // Validate request body
-  const validation = createIssueSchema.safeParse(body);
+  const validation = issueSchema.safeParse(body);
   // If not validated return an error
   if (!validation.success) {
     return NextResponse.json(validation.error.format(), { status: 400 });
