06aa2011a7c52e9905277f026d0d691850e575ab
Mark Slorach <hello@markslorach.com>
2024-10-10 16:12:37 +0100

    added edit button

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -4,6 +4,9 @@ import delay from "delay";
 import IssueStatusBadge from "@/app/components/IssueStatusBadge";
 import { Card } from "@/components/ui/card";
 import ReactMarkdown from "react-markdown";
+import { Button } from "@/components/ui/button";
+import { SquarePen } from "lucide-react";
+import Link from "next/link";
 
 type IssueDetailsPageProps = {
   params: {
@@ -13,20 +16,30 @@ type IssueDetailsPageProps = {
 
 const IssueDetailsPage = async ({ params }: IssueDetailsPageProps) => {
   const { issue } = await getIssue(params.id);
-//   await delay(1000);
+  //   await delay(1000);
 
   if (!issue) notFound();
 
   return (
-    <div>
-      <h1 className="text-3xl font-semibold">{issue.title}</h1>
-      <div className="flex space-x-3 mt-3 mb-10">
-        <IssueStatusBadge status={issue.status} />
-        <p>{issue.createdAt.toDateString()}</p>
+    <div className="grid sm:grid-cols-2 gap-5">
+      <div>
+        <h1 className="text-3xl font-semibold">{issue.title}</h1>
+        <div className="flex space-x-3 mt-3 mb-10">
+          <IssueStatusBadge status={issue.status} />
+          <p>{issue.createdAt.toDateString()}</p>
+        </div>
+        <Card className="p-4 prose max-w-3xl">
+          <ReactMarkdown>{issue.description}</ReactMarkdown>
+        </Card>
       </div>
-      <Card className="p-4 prose max-w-3xl">
-        <ReactMarkdown>{issue.description}</ReactMarkdown>
-      </Card>
+      <div>
+        <Link href={`/issues/${issue.id}/edit`}>
+          <Button>
+            <SquarePen className="w-4 h-4 mr-2" />
+            Edit Issue
+          </Button>
+        </Link>
+      </div>
     </div>
   );
 };
